c3a1d15dfb45ace81ee120be59da8f2c6a401efa
evis opari <eopari@gmail.com>
2024-04-09 21:04:18 +0000

    JS classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,49 +1,24 @@
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
 
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
 
-function sayHello(name) {
-  if (name === undefined) {
-    console.log("hello no name")
-  } else {
-    console.log("hello " + name)
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
   }
 }
-const name = 'Evis'
 
-console.log("---------------")
-console.log('Before set timeout')
-
-setTimeout(() => {
-  sayHello(name)
-}, 2000
-
-)
-
-console.log('after setTimeout')
-
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
-}
-
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
-}
-
-let k = 10
-
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+const g = new Greeter('visi')  // Put your name here if you like
+g.greet()
